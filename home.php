ee25bf2fcd187437dd8c3e3729e5380d66e8e01f
Deepanshu Arya <deepanshu.da85@gmail.com>
2021-10-16 18:41:11 +0530

    added logout

--- home.php
+++ home.php
@@ -16,7 +16,7 @@
 <div class="container rounded bg-white mt-5 mb-5">
     <div class="row">
         <div class="col-md-3 border-right">
-            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold"><?php echo $_SESSION['user_name'];?></span><span class="text-black-50"><?php echo $_SESSION['user_email'];?></span><span> </span></div>
+            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold"><?php echo $_SESSION['user_name'];?></span><span class="text-black-50"><?php echo $_SESSION['user_email'];?></span><span><a href="logout.php"><button class="btn btn-primary profile-button">Logout</button></a></span></div>
         </div>
         <div class="col-md-5 border-right">
             <div class="p-3 py-5">
